878adf3522ad698f06d23f238d8c7b89a01fca18
oshikawatkm <tkm05oshikawa12@gmail.com>
2022-02-14 13:04:04 +0900

    update

--- practice/sort/binary.py
+++ practice/sort/binary.py
@@ -1,12 +1,7 @@
+from cgi import print_directory
 from typing import List, NewType
 
 IndexNum = NewType('IndexNum', int)
-
-def linear_search(numbers: List[int], value: int) -> int:
-  for i in range(0, len(numbers)):
-    if numbers[i] == value:
-      return i
-  return -1
 
 def binary_search(numbers: List[int], value: int) -> int:
   def _binary_search(numbers: List[int], value: int, left: IndexNum, right: IndexNum) -> IndexNum:
@@ -17,7 +12,7 @@ def binary_search(numbers: List[int], value: int) -> int:
     if numbers[mid] == value:
       return mid
     elif numbers[mid] < value:
-      return _binary_search(numbers, value, mid + 1, right)
+      return _binary_search(numbers, value, mid+1, right)
     else:
       return _binary_search(numbers, value, left, mid + 1)
 
@@ -25,5 +20,5 @@ def binary_search(numbers: List[int], value: int) -> int:
 
 
 if __name__ == '__main__':
-  nums = [0, 1, 5, 7, 9, 11, 15, 20, 24]
+  nums = [1, 12, 15, 17, 35, 67, 72, 88, 89, 100]
   print(binary_search(nums, 15))
